be38f48e439acebf3cd1a3149e36e265b3927a4c
gmarkley-VI <gmarkley@redhat.com>
2020-02-17 18:22:43 -0500

    Added ENV reading for Username and Password.

--- functions/creds.go
+++ functions/creds.go
@@ -3,17 +3,28 @@ package functions
 import (
 	"fmt"
 	"golang.org/x/crypto/ssh/terminal"
+	"os"
 	"strings"
 )
 
-// Read in the Credentials from command line.
+// Read in the Credentials from command line or ENV variables
 func ReadCredentials() (string, string) {
-	fmt.Print("Enter Username: ")
-	username, _ := terminal.ReadPassword(0)
+	//Read in Env Variable if they exist )
+	username, exists := os.LookupEnv("USER")
+	if !exists {
+		fmt.Print("Enter Username: ")
+		username, _ := terminal.ReadPassword(0)
+		fmt.Printf("\n")
+		strings.TrimSpace(string(username))
+	}
 
-	fmt.Printf("\nPassword: ")
-	password, _ := terminal.ReadPassword(0)
-	fmt.Printf("\n")
+	password, exists := os.LookupEnv("JIRAPW")
+	if !exists {
+		fmt.Printf("Password: ")
+		password, _ := terminal.ReadPassword(0)
+		fmt.Printf("\n")
+		strings.TrimSpace(string(password))
+	}
 
-	return strings.TrimSpace(string(username)), strings.TrimSpace(string(password))
+	return username, password
 }
